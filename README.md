0b3988a35257b668921d4dc844e806352b834258
Badal <badal@NETURON17>
2023-04-06 16:09:54 +0530

    completed

---
+++ README.md
@@ -0,0 +1,8 @@
+# Show-Model-
+
+JS based, section revel by clicking buttons.
+
+Botons showed on the screen, by clicking any of botton you will see a popup that contain a specific data and hav a close botton.
+you can close this popup using close botton on top-right or by clicking anywere outside of the contaner.
+
+#link - http://cmsapp.ml/sm/
---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const showModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
+const open = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const close = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < showModal.length; i++)
+  showModal[i].addEventListener('click', open);
+
+closeModal.addEventListener('click', close);
+overlay.addEventListener('click', close);
+document.addEventListener('keydown', function (v) {
+  if (v.key === 'Escape' && !modal.classList.contains('hidden')) close();
+});
